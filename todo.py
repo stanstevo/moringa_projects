465b6329d091d7c9b8a674cdc981df3ac40f0ef2
stanstevo <stevemth@gmail.com>
2023-08-29 15:38:44 +0300

    implemented mark completed functionality

--- todo.py
+++ todo.py
@@ -8,3 +8,19 @@ def add_tasks(task_name: str) -> list:
     """
     tasks.append({"task_name": task_name, "completed": False})
     print("Task added!")
+
+def mark_completed(task_index: int) -> None:
+    """
+    Mark a task as completed.
+
+    Args:
+        task_index (int): Index of the task to be marked as completed.
+    """
+    if task_index >= 0 and task_index < len(tasks):
+        if tasks[task_index]["completed"]:
+            print("Task is already completed")
+        else:
+            tasks[task_index]["completed"] = True
+            print("Task is marked as completed")
+    else:
+        print("You have entered an invalid index")
